4e470e194b5d33bef1bbf36dbc8914d549ccce06
sreichholf <sreichholf@users.noreply.github.com>
2022-01-28 08:12:06 +0000

    Deploying to gh-pages from @ sreichholf/sreichholf.github.io@73137cf5d9588a60cf44f1f9247c5f59e2457a5b 🚀

---
+++ _src/index.js
@@ -0,0 +1,12 @@
+// CSS and SASS files
+import './index.scss';
+
+import { default as Tobi } from '@midzer/tobii'
+const tobi = new Tobi()
+
+// Remove the following lines to remove the darkmode js
+import Darkmode from 'darkmode-js'
+function addDarkmodeWidget() {
+  new Darkmode().showWidget()
+}
+window.addEventListener('load', addDarkmodeWidget)
